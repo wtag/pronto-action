5acbac86c1286a9fb09cc5352cb22dd92f4cede1
Subrato <subrato.sarker@welldev.io>
2022-08-18 15:28:01 +0600

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ SHA=$(jq --raw-output .head_commit.id "${GITHUB_EVENT_PATH}")
 OWNER=$(jq --raw-output .repository.owner.name "${GITHUB_EVENT_PATH}")
 REPO=$(jq --raw-output .repository.name "${GITHUB_EVENT_PATH}")
 HEAD_COMMIT_PULLS_URL="https://api.github.com/repos/${OWNER}/${REPO}/commits/${SHA}/pulls"
-PULL_IDS=$(curlGH $HEAD_COMMIT_PULLS_URL | jq --raw-output .[].number)
+PULL_IDS=$(curlGH $HEAD_COMMIT_PULLS_URL | jq --raw-output .[]."number")
 
 if [ -z "${PULL_IDS}" ]; then
   echo "No pull request found, bailing out"
