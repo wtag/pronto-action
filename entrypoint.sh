937d593263c66a54cc7b10442b8cb3f5b317ff4e
Subrato <subrato.sarker@welldev.io>
2022-12-22 19:39:50 +0600

    implemented

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,37 +3,15 @@
 set -eo pipefail
 
 export PRONTO_GITHUB_ACCESS_TOKEN="${1}"
-export GITHUB_REPO_FROM_ENV="${2}"
-export GITHUB_SHA_FROM_ENV="${3}"
-
-echo 'Start Printing GH ENV variables'
-echo $GITHUB_EVENT_NAME
-echo $GITHUB_REPOSITORY
-echo $GITHUB_SHA
 
 curlGH() {
   curl --silent -H "Accept: application/vnd.github.groot-preview+json" -H "Authorization: token ${PRONTO_GITHUB_ACCESS_TOKEN}" ${@}
 }
 
-SHA=$(jq --raw-output .head_commit.id "${GITHUB_EVENT_PATH}")
-OWNER=$(jq --raw-output .repository.owner.name "${GITHUB_EVENT_PATH}")
+SHA=$(jq --raw-output .after "${GITHUB_EVENT_PATH}")
+OWNER=$(jq --raw-output .repository.owner.login "${GITHUB_EVENT_PATH}")
 REPO=$(jq --raw-output .repository.name "${GITHUB_EVENT_PATH}")
-HEAD_COMMIT_PULLS_URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/commits/${GITHUB_SHA}/pulls"
-
-echo $GITHUB_EVENT_PATH
-echo $SHA
-echo $OWNER
-echo $REPO
-echo $HEAD_COMMIT_PULLS_URL
-
-echo $GITHUB_REPO_FROM_ENV
-echo $GITHUB_SHA_FROM_ENV
-
-echo "Attempting to print directly the repository value"
-
-REPO_INFO=github.repository
-echo $REPO_INFO
-
+HEAD_COMMIT_PULLS_URL="https://api.github.com/repos/${OWNER}/${REPO}/commits/${SHA}/pulls"
 PULL_IDS=$(curlGH $HEAD_COMMIT_PULLS_URL | jq --raw-output .[].number)
 
 if [ -z "${PULL_IDS}" ]; then
