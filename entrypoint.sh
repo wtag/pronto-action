bfcaa1167ba04ab352512acc05a41f17f16d0eae
Subrato <subrato.sarker@welldev.io>
2022-12-22 16:43:40 +0600

    added echo

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,6 +12,12 @@ SHA=$(jq --raw-output .head_commit.id "${GITHUB_EVENT_PATH}")
 OWNER=$(jq --raw-output .repository.owner.name "${GITHUB_EVENT_PATH}")
 REPO=$(jq --raw-output .repository.name "${GITHUB_EVENT_PATH}")
 HEAD_COMMIT_PULLS_URL="https://api.github.com/repos/${OWNER}/${REPO}/commits/${SHA}/pulls"
+echo $HEAD_COMMIT_PULLS_URL
+echo $SHA
+echo $OWNER
+echo $REPO
+echo $GITHUB_EVENT_PATH
+
 PULL_IDS=$(curlGH $HEAD_COMMIT_PULLS_URL | jq --raw-output .[].number)
 
 if [ -z "${PULL_IDS}" ]; then
